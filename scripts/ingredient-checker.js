44b80a8ee8082349d347de4cd70280bb04502258
Jake Abed <jakeabed@gmail.com>
2022-10-10 18:35:13 -0700

    Update ingredient-checker.js

--- scripts/ingredient-checker.js
+++ scripts/ingredient-checker.js
@@ -6,6 +6,6 @@ let ingredientInput = document.getElementById("ingredient-input");
 processListButton.addEventListener("click", updateResponseText, false);
 
 function updateResponseText() {
-    ingredientMessage.innerHTML = ingredientInput.value;
+    ingredientMessage.innerHTML = ingredientInput.value + " | Just so you know, this doesn't actually work right now. I'm just getting started.";
 }
 
