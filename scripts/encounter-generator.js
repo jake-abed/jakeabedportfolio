9914052d9c7e7a6e3c065320e1fdf7b1d3bcf081
Jake Abed <jakeabed@gmail.com>
2022-10-27 13:36:16 -0700

    Revision to Names Styles // Add Encounter Generator
    -Encounter generator
    -Styles fixed up a little.
    -Changes to nav names.
    -Layout changes.

---
+++ scripts/encounter-generator.js
@@ -0,0 +1,12 @@
+"use strict";
+/*
+Preferences Object - Contains the preferences from the user input.
+*/
+const encounterPreferences = { //Set to default values
+    setting: "random",
+    nonCombat: false,
+    lowDanger: true,
+    mediumDanger: true,
+    highDanger: true,
+    veryHighDanger: true,
+}
