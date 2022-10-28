ea3d91e9868436c4cd7ff0f2713628b202d4b2a2
Jake Abed <jakeabed@gmail.com>
2022-10-27 18:13:11 -0700

    Update Styles & Data Structure Tests

--- scripts/encounter-generator.js
+++ scripts/encounter-generator.js
@@ -8,6 +8,16 @@ const encounterPreferences = { //Set to default values
     extraHard: false,
 }
 
+let encounter001 = {
+    location: ["random", "city"],
+    combat: true,
+    extraHard: false,
+    title: "A shady merchant approaches...",
+    description:    "A small human man approaches the party claiming to be a merchant. He says he has small trinkets to peddle. When speaking as this character, be sure to ramble. When asked about his wares, he's going to be evasive and vague. Truthfully, he's trying to stall and pickpocket a random party member. Roll for sleight of hand plus 5. He'll try to steal up to 5 gold coins, three times. If he succeeds, he'll show the party some random junk then leave suddenly and angrily. If the party detects him, they get the reward!",
+    reward: "100 xp and 10 gold.",
+    dmNotes: "Adjust amount stolen, XP, and reward gold as needed for high level parties. Consider having him not steal from struggling players.",
+}
+
 const nonCombatEncounter = { //A sample noncombat encounter to see what I want to return?
     nonCombat: true,
     setting: undefined,//create a function to choose a setting based off of user input.
@@ -24,7 +34,7 @@ extraHardValue = document.getElementById("extra-hard");
 
 
 function logDumbMessage() {
-    return console.log(encounterPreferences);
+    return console.log(encounter001);
 }
 
 function updateEncounterPreferences() {
