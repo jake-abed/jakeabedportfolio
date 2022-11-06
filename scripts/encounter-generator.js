79d9ee2a242c8fab7741fc5cb63a883cf172ec5a
Jake Abed <jakeabed@gmail.com>
2022-11-05 20:38:40 -0700

    Spiralling, Then Fixing Problems

--- scripts/encounter-generator.js
+++ scripts/encounter-generator.js
@@ -1,4 +1,14 @@
 "use strict";
+const generateEncounterButton = document.getElementById("annoying-button"),
+      encounterSettingValue = document.getElementById("encounter-setting"),
+      nonCombatValue = document.getElementById("is-combat"),
+      crValue = document.getElementById("cr"),
+      numOfPlayersValue = document.getElementById("number-of-players"),
+      encounterTitleDisplay = document.getElementById("encounter-title"),
+      encounterDescriptionDisplay = document.getElementById("encounter-description"),
+      encounterEnemiesDisplay = document.getElementById("encounter-enemies"),
+      encounterRewardDisplay = document.getElementById("encounter-rewards"),
+      encounterDMNotesDisplay = document.getElementById("encounter-dm-notes");
 /*
 Preferences Object - Contains the preferences from the user input.
 */
@@ -7,7 +17,34 @@ const encounterPreferences = { //Set to default values
     combat: false,
     desiredCR: 1,
     numOfPlayers: 3,
-    calculatedCR: 6,
+    calculatedCR: function () {
+        return this.desiredCR * this.numOfPlayers * 2;
+    },
+    update: function () {
+        this.setting = encounterSettingValue.value;
+        this.combat = (nonCombatValue.value === "true");
+        this.desiredCR = Number(crValue.value);
+        this.numOfPlayers = Number(numOfPlayersValue.value);
+        this.maxEnemyCR = this.desiredCR * 8;
+        if (this.combat == true) {
+            this.possibleEnemyList = createPossibleEnemyList(this.maxEnemyCR, this.setting, "all");
+        }
+        return this;
+    },
+}
+
+const currentEncounter = {
+    title: null,
+    description: null,
+    requiredEnemy: null,
+    enemyType: null,
+    singleEnemy: null,
+    rewardTable: null,
+    dmNotes: null,
+    reward: function () {
+        return selectRandomArrayEntry(this.rewardTable);
+    },
+    enemies: null,
 }
 
 let loopBreakpoint = 200; //If this reaches zero, the random generation was too inefficient and errors out.
@@ -16,7 +53,6 @@ const masterEncounterArray = [
     {
         location: ["random", "city"],
         combat: false,
-        extraHard: false,
         title: "A shady merchant approaches...",
         description:    "A small human man approaches the party claiming to be a merchant. He says he has small trinkets to peddle. When speaking as this character, be sure to ramble. When asked about his wares, he's going to be evasive and vague. Truthfully, he's trying to stall and pickpocket a random party member. Roll for sleight of hand plus 5. He'll try to steal up to 5 gold coins, three times. If he succeeds, he'll show the party some random junk then leave suddenly and angrily. If the party detects him, they get the reward!",
         rewardTable: ["100 xp and 10 gold.", "100xp and 20 gold.", "100xp and 5 gold."],
@@ -29,7 +65,6 @@ const masterEncounterArray = [
     {
         location: ["random", "city", "outdoors"],
         combat: false,
-        extraHard: false,
         title: "A decrepit priest cries out for alms and prayers...",
         description: "The party notices the tragic cries of a worn-down, old priest nearby. He is begging for prayers and potentially alms of any kind. The priest will lament that too few people pay him any mind and that the world needs more positivity and prayer. He makes sure to mention that any charity will do. When asked, his god will be whatever the local god is. If you are uncertain, insert a generic sun god nearby. For the cloud sea setting, he's worshipping the First God. There are no skill checks here, just a chance for the players to interact with a sad old man. If the players are kind and give him something, he will thank them and promise to pray for them. If the players pray with him, he will lead them in a quick, positive prayer and send them on their way.",
         rewardTable: ["None, potentially clemency from the DM in the future.", "A small effigy of the priest's god."],
@@ -42,7 +77,6 @@ const masterEncounterArray = [
     {
         location: ["cloudsea"],
         combat: false,
-        extraHard: false,
         title: "An airship restaurant approaches...",
         description: "A lively restaurant, known as 'The Noodly Appendage' based out of an airship approaches the party. They serve noodles, fried chicken, and sky whale porridge. If the players engage with the proprietors, they get delicous food and a information about a famous restaurant in Polpizu (Polpran City) named 'Sumptuous Blizzard' operated by a Kenku chef known as Ogreel van Kreuzh.",
         reward: "No reward, just good food and info.",
@@ -52,7 +86,6 @@ const masterEncounterArray = [
     {
         location: ["random", "outdoors", "dungeon"],
         combat: false,
-        extraHard: false,
         title: "A glowing orb with a disembodied voice approaches...",
         description: "A glowing orb with a disembodied and monstrously booming voice approaches from behind a nearby object. Tree, coffin, building, whatever. It says that if you can answer its riddles and questions correctly, it will reward you handsomely. The first one is a riddle: 'We can be fast, we can be slow, We sometimes fly yet fall so low. We bring bout life, we can also kill.  Many & everywhere, we are hot, cold, moving, or still. Who are we?' The answer is 'We Are Water'. If they clown the light for riddle being mediocre, he suggests they come up with a better one. The second question is history check or theology check on the land. DC 15. For the Cloud Sea, the question is 'What year was the dissolution of the first Aarokocran Dominion?' The answer is '0 PD or 0 Post Dominion' as a kind of trick question. The final question is 'Three children have three loaves of bread each. They invite three more friends over, each with one loaf of bread. They then slice each loaf of bread into nine slices. They then invite over three more friends, then divide the bread evenly amongst them all. Assuming each child is only hungry enough to eat two slices of bread (there is some jam and butter as well), how many loaves are left over?' The answer is 4.",
         reward: "Ring Of Truths",
@@ -62,7 +95,6 @@ const masterEncounterArray = [
     {
         location: ["dungeon", "city"],
         combat: false,
-        extraHard: false,
         title: "A small gargoyle sits abjectly in the corner of the room.",
         description: "The tiniest gargoyle sits in the corner of the room, looking immensely gloomy. He says he lost his staff and can't prop himself menacingly on any parapets or columns. If the party searches the room, they'll notice that the gargoyle's staff is under a large pile of rubble. DC 10 perception. To clear the rubble, it'll require a strength check of DC 20 - 5 per participating player. A DC 15 perception check will reveal two opaline gems about a half inch in diameter in the rubble. They are the gargoyle's eyes.",
         reward: "If they keep the opals, they are worth 20gp each. If they return the opals, the gargoyle will offer to accompany them on their journey. He has 200 HP, can carry one item for the party, has 20 AC, but only deals 1d4 damager with a +5 attack modifier. He's very weak.",
@@ -72,7 +104,6 @@ const masterEncounterArray = [
     {
         location: ["random", "outdoors", "city", "cloudsea"],
         combat: false,
-        extraHard: false,
         title: "A bird shits on a random player's head.",
         description: "A large white and grey bird craps on one of the player's heads.",
         rewardTable: ["Poop.", "Nothing.", "A sense of pride and accomplishment."],
@@ -85,7 +116,6 @@ const masterEncounterArray = [
     {
         location: ["random", "city", "outdoors", "cloudsea"],
         combat: false,
-        extraHard: false,
         title: "A bird starts squawking and drops an envelope...",
         description: "A hawk-like bird drops a wax-sealed envelope with the players, then flies away. The envelope contains a brief letter saying, 'If you are reading this letter, my familiar has determined that you are a good candidate to assist with a task. We need help transporting a sensitive and fragile item. Please come to the Grand Cloister on the center of Polpran and ask for Miztrum the Eye.'",
         reward: "A potential quest.",
@@ -95,7 +125,6 @@ const masterEncounterArray = [
     {
         location: ["random", "outdoors", "dungeon"],
         combat: false,
-        extraHard: false,
         title: "A man is trapped under debris...",
         description: "A muscular, but extremely wounded man is trapped underneath debris. If outdoors, it's a tree or some stones. In a dungeon, it could be a trap or stone or a collapsed wall. Either way, a DC 20 strength check (lowered by 5 for each participating player) will free him. Or if the player's spend 2d4 hours, they can free him. Once freed, he will reward the players and start running away. He'll be muttering, 'Verlacht is gonna kill me...' while leaving. If the player's don't save him, he'll scream incessantly until out of earshot.",
         reward: "TBD, maybe 50 gold.",
@@ -105,7 +134,6 @@ const masterEncounterArray = [
     {
         location: ["random", "outdoors", "dungeon", "city", "cloudsea"],
         combat: true,
-        extraHard: false,
         title: "Test for combat logic and looping.",
         description: "How many iterations will it take to get this one?",
     },
@@ -113,7 +141,6 @@ const masterEncounterArray = [
     {
         location: ["random", "outdoors", "dungeon", "city"],
         combat: true,
-        extraHard: false,
         title: "Test 2 for combat logic and looping.",
         description: "How many iterations will it take to get this second one?",
     },
@@ -131,7 +158,7 @@ const masterEncounterArray = [
 /*masterCombatEncounterArray object template : 
     {
         title:
-        location:
+        locations:
         minCR:
         description:
         enemyType:
@@ -142,7 +169,48 @@ const masterEncounterArray = [
 
 */
 
-const masterCombatEncounterArray = [];
+const masterCombatEncounterArray = [
+    {
+        title: "A lone monster lunges from the shadow and attacks your party!",
+        locations: ["random", "city", "outdoors", "dungeon", "cloudsea"],
+        minCR: 4,
+        description: "Your party is just minding it's business doing their thing, where a single monster lunges out of nowhere. It targets a random player and starts with advantage.",
+        enemyType: "monster",
+        singleEnemy: true,
+        rewardTable: undefined,
+        dmNotes: "Feel free to sub out the monster for something more thematically appropriate."
+    },
+    {
+        title: "A whizzing sound grows...",
+        locations: ["cloudsea"],
+        minCR: 4,
+        description: "A harsh whizzing sound begins growing as a rabid animal plummets from the sky like a comet.",
+        enemyType: "animal",
+        singleEnemy: true,
+        rewardTable: undefined,
+        dmNotes: "If the enemy generated cannot fly, sub in something different."
+    },
+    {
+        title: "A group of random undead enemies attack!",
+        locations: ["random", "outdoors", "dungeon", "cloudsea"],
+        minCR: 4,
+        description: "What is this, Final Fantasy? It's just a ton of undead creatures attacking.",
+        enemyType: "undead",
+        singleEnemy: false,
+        rewardTable: undefined,
+        dmNotes: "This is a generic combat encounter for generic times."
+    },
+    {
+        title: "An assassin lunges out of the dark and attacks!",
+        locations: ["random", "city", "outdoors", "dungeon"],
+        minCR: 4,
+        description: "If the assassin is undetected, he gets advantage on the player he attacks. The assassin is human, swathed in blue-black cloth and leather, and utterly silent. If subdued before dying, he will reveal his goal.",
+        enemyType: "npc",
+        singleEnemy: false,
+        rewardTable: undefined,
+        dmNotes: "Make the assassin's goal work into a plot beat. Also, have each party member roll a perception check at the begining of the encounter. A 20 will reveal the assassin, preventing advantage."
+    },
+];
 
 /*
 Master enemy array template:
@@ -202,52 +270,30 @@ const masterEnemyArray = [
     {name: "Adult Black Dragon", potentialLocations: ["random", "city", "outdoors", "cloudsea", "dungeon"], enemyType: "dragon", cr: 112, xpVal: 11500}
 ]
 
-const generateEncounterButton = document.getElementById("annoying-button"),
-      encounterSettingValue = document.getElementById("encounter-setting"),
-      nonCombatValue = document.getElementById("is-combat"),
-      crValue = document.getElementById("cr"),
-      numOfPlayersValue = document.getElementById("number-of-players"),
-      encounterTitleDisplay = document.getElementById("encounter-title"),
-      encounterDescriptionDisplay = document.getElementById("encounter-description"),
-      encounterEnemiesDisplay = document.getElementById("encounter-enemies"),
-      encounterRewardDisplay = document.getElementById("encounter-rewards"),
-      encounterDMNotesDisplay = document.getElementById("encounter-dm-notes");
-
 function selectRandomArrayEntry(anArray) {
     return anArray[Math.floor(Math.random()*anArray.length)]
 }
 
-function createPossibleEnemyList(maxEnemyCR, location, enemyType) {
+function createPossibleEnemyList(enemyType = "all", enemyName = undefined) {
+    const maxEnemyCR = encounterPreferences.calculatedCR();
+    const location = encounterPreferences.setting;
     let i = 0;
-    let enemyListReturn = [];
+    let enemyList = [];
     while (i < masterEnemyArray.length) {
         let currentMasterEnemyArrayEntry = masterEnemyArray[i];
-        if (currentMasterEnemyArrayEntry.cr <= maxEnemyCR && currentMasterEnemyArrayEntry.potentialLocations.includes(location) == true && (currentMasterEnemyArrayEntry.enemyType == enemyType || enemyType == "all")) {
-            enemyListReturn.push(masterEnemyArray[i]);
+        if (currentMasterEnemyArrayEntry.cr <= maxEnemyCR && currentMasterEnemyArrayEntry.potentialLocations.includes(location) == true && (currentMasterEnemyArrayEntry.enemyType == enemyType || enemyType == "all") && (enemyName == undefined || currentMasterEnemyArrayEntry.name == enemyName)) {
+            enemyList.push(masterEnemyArray[i]);
             i++;
         } else {
             i++;
         }
     }
-    return enemyListReturn;
+    return enemyList;
 }
 
-
-
-function updateEncounterPreferences() {
-    encounterPreferences.setting = encounterSettingValue.value;
-    encounterPreferences.combat = (nonCombatValue.value === "true");
-    encounterPreferences.desiredCR = Number(crValue.value);
-    encounterPreferences.numOfPlayers = Number(numOfPlayersValue.value);
-    encounterPreferences.calculatedCR = 2 * encounterPreferences.desiredCR * encounterPreferences.numOfPlayers;
-    encounterPreferences.maxEnemyCR = encounterPreferences.desiredCR * 8;
-    encounterPreferences.possibleEnemyList = createPossibleEnemyList(encounterPreferences.maxEnemyCR, encounterPreferences.setting, "all");
-    console.log(encounterPreferences);
-}
-
-/* Old Random Encounter Generator
-function randomEncounter() {
-    const encounterPick = encounterMap.get(Math.floor((Math.random()*10)));
+/*
+function generateEncounter() {
+    const encounterPick = (Math.floor((Math.random()*10)));
     const encounterPickLocation = encounterPick.location; 
     const encounterPickCombat = encounterPick.combat;
     if (!encounterPickLocation.includes(encounterPreferences.setting)) {
@@ -267,5 +313,9 @@ function randomEncounter() {
     return;
 }
 */
-generateEncounterButton.addEventListener("click", updateEncounterPreferences, false);
-//generateEncounterButton.addEventListener("click", randomEncounter, false);
+function createRandomEncounter() {
+    encounterPreferences.update();
+    return console.log(encounterPreferences);
+}
+
+generateEncounterButton.addEventListener("click", createRandomEncounter, false);
